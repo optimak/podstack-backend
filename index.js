cc82e9aa84a8a2d07c3d3e8a4ea81f2d5a72ef28
Chi Aghaizu <chiaghaizu@gmail.com>
2024-03-22 09:57:50 -0400

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const PORT = 8080;
+const cors = require("cors");
+app.use(cors());
 // const PORT = process.env.PORT || 5050;
 app.get('/', (req, res) => {
     res.send('Welcome to my API');
