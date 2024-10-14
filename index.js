9649c0544064b8895d969152c50c6a59b360500d
Chi Aghaizu <chiaghaizu@gmail.com>
2024-10-14 18:08:12 -0400

    add port changes and npm start, and python dotenv version update

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express');
+// const app = express();
+// require("dotenv").config();
+// const PORT = 5050;
+// const cors = require("cors");
+// app.use(cors());
+// // const PORT = process.env.PORT || 5050;
+// app.get('/', (req, res) => {
+//     res.send('Welcome to my API');
+//   });
+// const podcasterRoutes = require('./routes/routes');
+
+// // all users routes
+// app.use('/podcasters', podcasterRoutes);
+
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server is running on http://0.0.0.0:${PORT}`);
+// });
+
 const app = express();
 require("dotenv").config();
-const PORT = 5050;
 const cors = require("cors");
 app.use(cors());
-// const PORT = process.env.PORT || 5050;
+
+// Use APP_PORT for your API server or default to 5050
+const APP_PORT = process.env.APP_PORT || 5050;
+
 app.get('/', (req, res) => {
     res.send('Welcome to my API');
-  });
+});
+
 const podcasterRoutes = require('./routes/routes');
 
-// all users routes
+// All users routes
 app.use('/podcasters', podcasterRoutes);
 
-app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+// Use APP_PORT for the server
+app.listen(APP_PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${APP_PORT}`);
 });
