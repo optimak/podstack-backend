25b538d38f18d57cf030a201fb72130238b7edb3
Chi Aghaizu <chiaghaizu@gmail.com>
2024-10-14 21:39:36 -0400

    add port changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const cors = require("cors");
 app.use(cors());
 
 // Use APP_PORT for your API server or default to 5050
-const APP_PORT = process.env.APP_PORT || 5050;
+const PORT = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
     res.send('Welcome to my API');
@@ -35,6 +35,6 @@ const podcasterRoutes = require('./routes/routes');
 app.use('/podcasters', podcasterRoutes);
 
 // Use APP_PORT for the server
-app.listen(APP_PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${APP_PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
